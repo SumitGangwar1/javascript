7088036effcee205a2890591cf0fd8f0c816f61b
Sumit Gangwar <sumitgangwar0112@gmail.com>
2026-09-17 20:58:09 +0530

    second change

---
+++ script.js
@@ -0,0 +1,46 @@
+// let a =10;
+// let  b =20;
+// console.log(a+b)
+
+
+
+// console.log(a)
+// console.warn("somethig went wrong")
+//  console.error("error 404")
+ 
+// var
+
+// var a = 10;
+
+// console.log(a)
+
+// var a = 20;
+// console.log(a)
+
+// a = 30;
+// console.log(a)
+
+//let
+
+// let a = 10;
+// console.log(a)
+// a =20;
+// console.log(a)
+
+// var c =20;
+// if(true){
+//     let y =30;
+//     let x =40;
+//      console.log(y,x)
+//     // console.log(c)
+// }
+
+//     console.log(c)
+
+let h = " hello"
+console.log(typeof(h))
+let v = 45;
+console.log(typeof(v))
+let d = true;
+console.log(typeof(d))
+  
